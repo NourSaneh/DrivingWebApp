d84c656cb173b87799076a6c5d102628bdfa0adb
NourSaneh <noursaneh34@gmail.com>
2025-11-30 02:44:50 +0400

    studyguide fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
-export const API_BASE =
-  import.meta.env.PROD
-    ? "https://driving-web-app.vercel.app"
-    : "https://driving-web-app.vercel.app";
+export const API_BASE = import.meta.env.PROD
+  ? "https://driving-web-app.vercel.app"
+  : "http://localhost:5174";
