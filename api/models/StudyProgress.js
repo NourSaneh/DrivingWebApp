d5cf378a27c7472fd5d95862cd353418831ad199
NourSaneh <noursaneh34@gmail.com>
2025-11-29 12:38:04 +0400

    vercela backend layout

--- api/models/StudyProgress.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const StudyProgressSchema = new mongoose.Schema({
-  userId: String,
-  completedTopics: [String],
-});
-
-export default mongoose.models.StudyProgress ||
-  mongoose.model("StudyProgress", StudyProgressSchema);
